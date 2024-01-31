95adf63fabf293d341a5c30b87f335d00557d14c
Gui <guillaume.bernard.pro@gmail.com>
2024-01-31 23:08:08 +0100

    Material theme colors + navbar

--- app/src/main/java/com/guillaume/bernard/mombookshelf/MainActivity.kt
+++ app/src/main/java/com/guillaume/bernard/mombookshelf/MainActivity.kt
@@ -3,14 +3,11 @@ package com.guillaume.bernard.mombookshelf
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
@@ -18,28 +15,24 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.GenericShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
-import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Check
-import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.rounded.AccountCircle
+import androidx.compose.material.icons.rounded.Home
 import androidx.compose.material.icons.rounded.Search
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.BottomAppBarDefaults
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
@@ -52,28 +45,26 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.zIndex
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
 import com.bumptech.glide.integration.compose.placeholder
 import com.guillaume.bernard.mombookshelf.model.Book
-import com.guillaume.bernard.mombookshelf.ui.theme.*
-import com.guillaume.bernard.mombookshelf.ui.theme.MomBookshelfTheme
-import com.guillaume.bernard.mombookshelf.ui.theme.TextPrimary
+import com.guillaume.bernard.mombookshelf.ui.theme.AppTheme
 import com.guillaume.bernard.mombookshelf.ui.theme.libreCaslonTextFamily
+import com.guillaume.bernard.mombookshelf.ui.theme.ralewayFamily
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MomBookshelfTheme {
+            AppTheme {
                 MainView()
             }
         }
@@ -95,62 +86,66 @@ fun MainView() {
             }
         },
         bottomBar = {
-            BottomAppBar(
-                actions = {
-                    IconButton(onClick = { /* do something */ }) {
-                        Icon(
-                            Icons.Default.AccountCircle,
-                            contentDescription = "Localized description"
-                        )
-                    }
-                    IconButton(onClick = { /* do something */ }) {
-                        Icon(
-                            Icons.Filled.Edit,
-                            contentDescription = "Localized description",
-                        )
-                    }
-                    IconButton(onClick = { /* do something */ }) {
-                        Icon(
-                            Icons.Filled.AccountBox,
-                            contentDescription = "Localized description",
-                        )
-                    }
-                },
-                floatingActionButton = {
-                    FloatingActionButton(
-                        onClick = { /* do something */ },
-                        containerColor = BottomAppBarDefaults.bottomAppBarFabColor,
-                        elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation()
-                    ) {
-                        Icon(Icons.Filled.Add, "Localized description")
-                    }
-                }
 
-            )
+            NavigationBar {
+                NavigationBarItem(selected = false, onClick = {},
+                    icon = {
+                        Icon(
+                            Icons.Rounded.Home,
+                            contentDescription = "Home"
+                        )
+                    },
+                    label = { Text("Home") }
+                )
+                NavigationBarItem(selected = false, onClick = {},
+                    icon = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_book),
+                            contentDescription = "Collection"
+                        )
+                    },
+                    label = { Text("Collection") })
+                NavigationBarItem(selected = false, onClick = {},
+                    icon = {
+                        Icon(
+                            Icons.Rounded.AccountCircle,
+                            contentDescription = "Profile"
+                        )
+                    },
+                    label = { Text(text = "Profile") }
+                )
+            }
         },
-        containerColor = Background
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { /* do something */ },
+            ) {
+                Icon(Icons.Filled.Add, "Localized description")
+            }
+        }
+
     ) { innerPadding ->
         Column(
             modifier = Modifier
                 .padding(innerPadding)
-                .fillMaxSize()
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.Start
         ) {
             var searchText by remember { mutableStateOf("") }
             TextField(
                 value = searchText,
-                placeholder = { Text(text = "Titre, auteur...", color = TextLight) },
+                placeholder = { Text(text = "Titre, auteur...") },
                 singleLine = true,
                 leadingIcon = {
                     Icon(
                         Icons.Rounded.Search,
                         contentDescription = null,
-                        tint = TextLight
                     )
                 },
                 onValueChange = { searchText = it },
                 shape = RoundedCornerShape(10.dp),
                 colors = TextFieldDefaults.textFieldColors(
-                    containerColor = SearchTextBackground,
                     focusedIndicatorColor = Color.Transparent,
                     unfocusedIndicatorColor = Color.Transparent,
                     disabledIndicatorColor = Color.Transparent
@@ -166,43 +161,57 @@ fun MainView() {
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
                 Text(
-                    text = "Derniers ajouts",
+                    text = "Dernier ajout",
                     fontFamily = libreCaslonTextFamily,
                     fontSize = 18.sp,
                     fontWeight = FontWeight.Bold,
-                    color = TextPrimary
                 )
                 Text(
                     text = "voir plus",
                     modifier = Modifier.align(Alignment.CenterVertically),
                     fontFamily = ralewayFamily,
                     fontWeight = FontWeight.Bold,
-                    color = ButtonNeutral,
                     fontSize = 13.sp,
                 )
             }
 
             Card(
-                modifier = Modifier.padding(24.dp),
+                modifier = Modifier.padding(horizontal = 24.dp),
                 shape = MaterialTheme.shapes.medium
             ) {
-                Column(
-                    modifier = Modifier.padding(16.dp)
+
+                Row(
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .fillMaxWidth()
                 ) {
-                    Row(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.Center) {
-                        BookView(book = SampleData.books.first())
+                    BookView(book = SampleData.books.first())
+                    Column(
+                        modifier = Modifier
+                            .padding(start = 12.dp)
+                            .fillMaxWidth(),
+                        verticalArrangement = Arrangement.Top
+                    ) {
+                        Text(
+                            text = "Book Title",
+                            style = MaterialTheme.typography.titleLarge,
+                            modifier = Modifier.padding(vertical = 8.dp)
+                        )
+                        Text(
+                            text = "Jean Neige",
+                            style = MaterialTheme.typography.labelMedium
+                        )
+                        Text(
+                            text = "2006",
+                            style = MaterialTheme.typography.labelMedium
+                        )
+                        Text(
+                            modifier = Modifier.padding(top = 6.dp),
+                            text = "Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet. Lormz dhiuoz zeir nknkigh zaih gi zehugig ihhharhi.",
+                            style = MaterialTheme.typography.bodySmall,
+                            color = Color.Gray
+                        )
                     }
-                    Text(
-                        text = "Card Title",
-                        style = MaterialTheme.typography.titleLarge,
-                        modifier = Modifier.padding(vertical = 8.dp)
-                    )
-                    Text(
-                        text = "This is some sample text for the card.",
-                        style = MaterialTheme.typography.bodyMedium
-                    )
                 }
             }
 //            Bookshelf(SampleData.books)
@@ -217,14 +226,12 @@ fun MainView() {
                     fontFamily = libreCaslonTextFamily,
                     fontSize = 18.sp,
                     fontWeight = FontWeight.Bold,
-                    color = TextPrimary
                 )
                 Text(
                     text = "voir plus",
                     modifier = Modifier.align(Alignment.CenterVertically),
                     fontFamily = ralewayFamily,
                     fontWeight = FontWeight.Bold,
-                    color = ButtonNeutral,
                     fontSize = 13.sp,
                 )
             }
@@ -236,9 +243,6 @@ fun MainView() {
                         Modifier
                             .size(180.dp, 100.dp)
                             .padding(end = 8.dp),
-                        colors = CardDefaults.cardColors(
-                            containerColor = genreDefaultColors.random()
-                        )
                     ) {
                         Text(
                             text = it,
@@ -246,7 +250,6 @@ fun MainView() {
                             fontFamily = libreCaslonTextFamily,
                             fontSize = 16.sp,
                             fontWeight = FontWeight.Bold,
-                            color = Color.White
                         )
                     }
                 }
@@ -264,50 +267,35 @@ fun Toolbar(modifier: Modifier = Modifier) {
         modifier = modifier,
         fontFamily = libreCaslonTextFamily,
         fontWeight = FontWeight.Bold,
-        color = TextPrimary,
         textAlign = TextAlign.Center
     )
-}
-
-@Composable
-fun Bookshelf(books: Array<Book>) {
-    Column {
-        LazyRow(
-            modifier = Modifier
-        ) {
-            item { Spacer(modifier = Modifier.padding(start = 60.dp)) }
-            items(books) {
-                BookView(it)
-            }
-        }
-    }
 }
 
 @OptIn(ExperimentalGlideComposeApi::class)
 @Composable
 fun BookView(book: Book) {
-    Box(modifier = Modifier.padding(end = 10.dp)
+    Box(
+        modifier = Modifier.padding(end = 10.dp)
     ) {
         Box(
             modifier = Modifier
                 .size(height = 180.dp, width = 130.dp)
                 .offset(4.dp, (-2).dp)
-                .clip(RoundedCornerShape(10.dp, 10.dp, 0.dp, 0.dp))
-                .background(BookPages)
+                .clip(RoundedCornerShape(10.dp))
+                .background(Color.White)
         )
 
         Box(
             modifier = Modifier
                 .size(height = 180.dp, width = 130.dp)
-                .clip(RoundedCornerShape(10.dp, 10.dp, 0.dp, 0.dp))
-                .background(bookDefaultCovers.random())
+                .clip(RoundedCornerShape(10.dp))
         ) {
             GlideImage(
                 model = "https://cdn1.booknode.com/book_cover/72/full/1984-72084.jpg",
                 loading = placeholder(R.drawable.ic_launcher_foreground),
                 contentDescription = null,
                 contentScale = ContentScale.FillWidth,
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier.fillMaxSize()
             )
         }
     }
@@ -319,7 +307,7 @@ fun BookView(book: Book) {
 )
 @Composable
 fun MainViewPreview() {
-    MomBookshelfTheme {
+    AppTheme {
         MainView()
     }
 }
